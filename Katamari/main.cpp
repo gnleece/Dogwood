cbac0eb5eb8f4cde41c8448645de7ca70b62f082
Gwynneth Leece <gleece@gmail.com>
2013-09-22 20:52:08 -0700

    Cleanup, adding GLU, drawing a sphere

---
+++ Katamari/main.cpp
@@ -0,0 +1,97 @@
+// main.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#define GLFW_INCLUDE_GLU
+#include <GLFW/glfw3.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+GLFWwindow* Setup();
+void Cleanup(GLFWwindow* window);
+void Render(GLFWwindow* window);
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main(void)
+{
+	GLFWwindow* window = Setup();
+    
+    while (!glfwWindowShouldClose(window))
+    {
+		Render(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+	Cleanup(window);
+    exit(EXIT_SUCCESS);
+}
+
+GLFWwindow* Setup()
+{
+	glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+	glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+	return window;
+}
+
+void Cleanup(GLFWwindow* window)
+{
+	glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void Render(GLFWwindow* window)
+{
+	// GLFW TEST CODE:
+	float ratio;
+    int width, height;
+    
+	glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+
+	GLUquadricObj* quad;
+    quad = gluNewQuadric();
+	gluSphere(quad, 0.5, 20, 20);
+	gluDeleteQuadric(quad);
+
+    glEnd();
+}
