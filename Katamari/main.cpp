2cc3d657d5d182684480ffa11ce06536e8736b12
Gwynneth Leece <gleece@gmail.com>
2013-09-28 17:41:17 -0700

    Lighting & perspective test code

--- Katamari/main.cpp
+++ Katamari/main.cpp
@@ -42,7 +42,7 @@ GLFWwindow* Setup()
     if (!glfwInit())
         exit(EXIT_FAILURE);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Katamari", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -51,6 +51,32 @@ GLFWwindow* Setup()
 
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
+
+    // Set up lighting
+    GLfloat light_position[] = {1.0f,1.0f,1.0f,0.0f};
+    GLfloat light_color[]={ 1.0f, 1.0f, 1.0f, 1.0f };
+    GLfloat ambient_color[]={ 0.4f, 0.4f, 0.4f, 1.0f };
+
+    glShadeModel(GL_SMOOTH);
+
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position );
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient_color );
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_color );
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_NORMALIZE);
+
+    float ratio;
+    int width, height;
+
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float) height;
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, width, height);
+    gluPerspective(40.0, ratio, 0.1, 1000.0);
 
     return window;
 }
@@ -63,20 +89,31 @@ void Cleanup(GLFWwindow* window)
 
 void Render(GLFWwindow* window)
 {
-    // GLFW TEST CODE:
-    float ratio;
-    int width, height;
-    
-    glfwGetFramebufferSize(window, &width, &height);
-    ratio = width / (float) height;
-    glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
-
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glPushMatrix();
+
+    // Set the initial camera
+    glTranslated(0.0, 0, -5.0);
+    glRotated(10,1.0,0.0,0.0);
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    // Position the camera
+    float m_zoom = 1.0f;
+    glScaled(m_zoom,m_zoom,m_zoom);
+    //glMultMatrixd(m_camera.transpose().begin());
+
+    // Position the light source
+    GLfloat light_position[] = {1.0,1.0,1.0,0.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position );
+
+    // DRAW SCENE
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // GLFW TEST CODE:
     glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
     glBegin(GL_TRIANGLES);
     glColor3f(1.f, 0.f, 0.f);
@@ -86,10 +123,15 @@ void Render(GLFWwindow* window)
     glColor3f(0.f, 0.f, 1.f);
     glVertex3f(0.f, 0.6f, 0.f);
 
+    // test sphere
     GLUquadricObj* quad;
     quad = gluNewQuadric();
-    gluSphere(quad, 0.5, 20, 20);
+    gluSphere(quad, 0.3, 20, 20);
     gluDeleteQuadric(quad);
+
+    // Cleanup
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
 
     glEnd();
 }
