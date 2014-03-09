e8b117f6bbd20db7582076e293f0af9726e8f620
Gwynneth Leece <gleece@gmail.com>
2014-03-09 11:50:12 -0700

    Shader lighting cleanup

--- Katamari/Engine/Shaders/FragmentShader.glsl
+++ Katamari/Engine/Shaders/FragmentShader.glsl
@@ -1,10 +1,12 @@
 #version 150
 
+in vec3 Position_worldspace;
+in vec3 LightPosition_worldspace;
+
 in vec3 Normal_cameraspace;
 in vec3 LightDirection_cameraspace;
-in vec3 Position_worldspace;
 in vec3 EyeDirection_cameraspace;
-in float Distance;
+
 in vec3 Color;
 in vec2 Texcoord;
 
@@ -14,28 +16,27 @@ uniform sampler2D tex;
 
 void main() 
 {
-    // Normal of the computed fragment, in camera space
-	vec3 n = normalize( Normal_cameraspace );
+    // material colors
+    vec4 MaterialDiffuseColor	= texture(tex, Texcoord)*vec4(Color, 1.0);
+    vec4 MaterialAmbientColor	= vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
+    vec4 MaterialSpecularColor	= vec4(1.0,1.0,1.0,1.0);
 
-	// Direction of the light (from the fragment to the light)
-	vec3 l = normalize( LightDirection_cameraspace );
-
-	float cosTheta = clamp( dot( n, l ), 0,1 );
+    // cosine of angle between normal and light direction, for diffuse lighting:
+    vec3 n = normalize(Normal_cameraspace);
+    vec3 l = normalize(LightDirection_cameraspace);
+    float cosTheta = clamp(dot(n, l), 0,1);
  
-	vec4 MaterialDiffuseColor = texture(tex, Texcoord)*vec4(Color, 1.0);
-	vec4 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
-	vec4 MaterialSpecularColor = vec4(1.0,1.0,1.0,1.0);//MaterialDiffuseColor;
+    // cosine of angle between the eye vector and the reflect vector, for specular lighting:
+    vec3 E = normalize(EyeDirection_cameraspace);
+    vec3 R = reflect(-l,n);
+    float cosAlpha = clamp(dot(E,R), 0,1);
 
-	// Eye vector (towards the camera)
-	vec3 E = normalize(EyeDirection_cameraspace);
-	// Direction in which the triangle reflects the light
-	vec3 R = reflect(-l,n);
-	// Cosine of the angle between the Eye vector and the Reflect vector,
-	// clamped to 0
-	//  - Looking into the reflection -> 1
-	//  - Looking elsewhere -> < 1
-	float cosAlpha = clamp( dot( E,R ), 0,1 );
- 
-	//TODO light color, light power, ambient as parameter
-	outColor = MaterialAmbientColor + 15*MaterialDiffuseColor*cosTheta / (Distance*Distance) + 150 * MaterialSpecularColor * pow(cosAlpha,5) / (Distance*Distance);
+    // distance to the light
+    float distanceSqrd = pow(distance(Position_worldspace, LightPosition_worldspace),2);
+
+    // combine all lighting types for final color
+    //TODO light color, light power, ambient as parameter
+    outColor =	MaterialAmbientColor + 
+                15 * MaterialDiffuseColor * cosTheta / distanceSqrd + 
+                150 * MaterialSpecularColor * pow(cosAlpha,5) / distanceSqrd;
 }
--- Katamari/Engine/Shaders/VertexShader.glsl
+++ Katamari/Engine/Shaders/VertexShader.glsl
@@ -5,11 +5,13 @@ in vec3 normal;
 in vec3 color;
 in vec2 texcoord;
 
+out vec3 Position_worldspace;
+out vec3 LightPosition_worldspace;
+
 out vec3 Normal_cameraspace;
 out vec3 LightDirection_cameraspace;
-out vec3 Position_worldspace;
 out vec3 EyeDirection_cameraspace;
-out float Distance;
+
 out vec3 Color;
 out vec2 Texcoord;
 
@@ -22,24 +24,23 @@ uniform vec3 lightpos;
 
 void main()
 {
-	gl_Position = proj*view*model*vec4(position, 1.0);
+    gl_Position = proj*view*model*vec4(position, 1.0);
 
-	Color = color;
-	Texcoord = texcoord;
- 
-	// Position of the vertex, in worldspace : model * position
-	Position_worldspace = (model * vec4(position,1)).xyz;
-	Distance = distance(Position_worldspace, lightpos);
- 
-	// Vector that goes from the vertex to the camera, in camera space.
-	// In camera space, the camera is at the origin (0,0,0).
-	vec3 vertexPosition_cameraspace = ( view * model * vec4(position,1)).xyz;
-	EyeDirection_cameraspace = vec3(0,0,0) - vertexPosition_cameraspace;
- 
-	// Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
-	vec3 LightPosition_cameraspace = ( view * vec4(lightpos,1)).xyz;
-	LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
- 
-	// Normal of the the vertex, in camera space
-	Normal_cameraspace = ( view * model * vec4(normal,0)).xyz; // TODO Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
+    Color = color;
+    Texcoord = texcoord;
+    LightPosition_worldspace = lightpos;
+
+    // Position of vertex in worldspace
+    Position_worldspace = (model * vec4(position,1)).xyz;
+
+    // Vector from vertex to camera, in camera space
+    vec3 Position_cameraspace = (view * model * vec4(position,1)).xyz;
+    EyeDirection_cameraspace = vec3(0,0,0) - Position_cameraspace;
+
+    // Vector from vertex to light, in camera space
+    vec3 LightPosition_cameraspace = (view * vec4(lightpos,1)).xyz;
+    LightDirection_cameraspace = LightPosition_cameraspace + EyeDirection_cameraspace;
+
+    // Normal of vertex in camera space
+    Normal_cameraspace = ( view * model * vec4(normal,0)).xyz; // TODO Only correct if ModelMatrix does not scale the model ! Use its inverse transpose if not.
 }
