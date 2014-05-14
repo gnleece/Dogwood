3232355009f3f6e57fbcb855df0f265329a2a273
Gwynneth Leece <gleece@gmail.com>
2014-05-13 22:07:49 -0700

    Clean up unnecessary Mesh member variables

--- Katamari/Engine/Rendering/Mesh.cpp
+++ Katamari/Engine/Rendering/Mesh.cpp
@@ -12,37 +12,38 @@
 
 Mesh::Mesh(std::string path)
 {
+    m_drawMode = GL_TRIANGLES;
+
+    std::vector<Vector3> positions;
+    std::vector<Vector3> normals;
+    std::vector<Vector2> uvs;
+    std::vector<GLuint>  indices;
+
     LoadIndexedModel(path, positions, normals, uvs, indices);
-    m_vertexPositionData = positions[0].Start();
-    m_vertexNormalData = normals[0].Start();
-    m_vertexUVData = uvs[0].Start();
 
     m_vertexCount = positions.size();
-
-    m_elementData = indices.data();
-    m_elementDataCount = indices.size();
-
-    m_drawMode = GL_TRIANGLES;
+    m_indexedVertexCount = indices.size();
 
     // bind buffer data
     glGenVertexArrays(1, &m_vao);
     glBindVertexArray(m_vao);
 
+    // upload vertex data
     glGenBuffers(1, &m_vboPosition);
     glBindBuffer(GL_ARRAY_BUFFER, m_vboPosition);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 3, m_vertexPositionData, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 3, positions[0].Start(), GL_STATIC_DRAW);
 
     glGenBuffers(1, &m_vboNormal);
     glBindBuffer(GL_ARRAY_BUFFER, m_vboNormal);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 3, m_vertexNormalData, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 3, normals[0].Start(), GL_STATIC_DRAW);
 
     glGenBuffers(1, &m_vboUV);
     glBindBuffer(GL_ARRAY_BUFFER, m_vboUV);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 2, m_vertexUVData, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*m_vertexCount * 2, uvs[0].Start(), GL_STATIC_DRAW);
 
     glGenBuffers(1, &m_ebo);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*m_elementDataCount, m_elementData, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint)*m_indexedVertexCount, indices.data(), GL_STATIC_DRAW);
 }
 
 void Mesh::Render(Matrix4x4& transform, Material* material)
@@ -53,7 +54,7 @@ void Mesh::Render(Matrix4x4& transform, Material* material)
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ebo);
 
         material->ApplyMaterial(m_vboPosition, m_vboNormal, m_vboUV, transform);
-        glDrawElements(m_drawMode, m_elementDataCount, GL_UNSIGNED_INT, 0);
+        glDrawElements(m_drawMode, m_indexedVertexCount, GL_UNSIGNED_INT, 0);
         material->UnapplyMaterial();
     }
 }
--- Katamari/Engine/Rendering/Mesh.h
+++ Katamari/Engine/Rendering/Mesh.h
@@ -19,26 +19,14 @@ public:
     void Delete();
 
 private:
-    GLint       m_uniModel;
-
     GLuint      m_vao;
     GLuint      m_vboPosition;
     GLuint      m_vboNormal;
     GLuint      m_vboUV;
     GLuint      m_ebo;
 
-    GLfloat*    m_vertexPositionData;
-    GLfloat*    m_vertexNormalData;
-    GLfloat*    m_vertexUVData;
     GLsizei     m_vertexCount;
-
-    GLuint*     m_elementData;
-    GLsizei     m_elementDataCount;
+    GLsizei     m_indexedVertexCount;
 
     GLenum      m_drawMode;
-
-    std::vector<Vector3> positions; // TODO don't need to store these
-    std::vector<Vector3> normals;
-    std::vector<Vector2> uvs;
-    std::vector<GLuint>  indices;
 };
