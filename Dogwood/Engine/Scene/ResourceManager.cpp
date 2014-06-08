2b44d9fe4026acc1c24ddc07cef04b55a2760aa3
Gwynneth Leece <gleece@gmail.com>
2014-06-07 17:55:50 -0700

    TinyXML2 library for XML parsing

--- Dogwood/Engine/Scene/ResourceManager.cpp
+++ Dogwood/Engine/Scene/ResourceManager.cpp
@@ -1,8 +1,14 @@
 #include "ResourceManager.h"
 
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
+
 void ResourceManager::Startup()
 {
     // Load Resources XML file and build resource lookup table
+    XMLDocument doc;
+    doc.LoadFile("Scenes\\Resources.xml");
 }
 
 void ResourceManager::Shutdown()
