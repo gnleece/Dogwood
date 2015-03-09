fc013d00c6317aa84da458142972f03740dd9187
Gwynneth Leece <gleece@gmail.com>
2015-03-08 20:14:04 -0700

    Build Schemas script processes serializable members and writes schema to XML file

--- Editor/Scripts/BuildSchemas.py
+++ Editor/Scripts/BuildSchemas.py
@@ -1,9 +1,15 @@
 import os
+import xml.etree.cElementTree as ET
 
 testPath = "c:\\Users\\Gwynneth\\Coding\\Dogwood\\Game\\Assets\\Scripts\\SpinningComponent.h"
 
 START_REGION_STRING = "#pragma region Serializable"
 END_REGION_STRING = "#pragma endregion"
+
+
+def GetScriptList(projectFilepath):
+    return { 12345 : testPath }
+
 
 def GetSerializableMembers(filename):
     with open(filename) as f:
@@ -29,25 +35,40 @@ def GetSerializableMembers(filename):
             if (line.startswith(END_REGION_STRING)):
                 foundEndRegion = True
                 break
-            serializableLines.append(line.lstrip().rstrip())
+            serializableLines.append(line.lstrip().rstrip(";\n"))
             line = f.readline()
 
         if (not foundEndRegion):
             print("Error: could not find end of Serializable region.")
             return
 
-        print("SERIALIZABLE LINES:")
-        for l in serializableLines:
-            print (l)
-
         return serializableLines
 
 
-def ProcessSerializableMembers(memberLines):
-    for l in memberLines:
-        tokens = l.split(" ");
-        print(tokens)
+def BuildSchemas(projectFilepath):
+    # Create root XML elemnt
+    rootXML = ET.Element("Scripts")
 
-            
-lines = GetSerializableMembers(testPath)
-ProcessSerializableMembers(lines)
+    # Get script list from project file
+    scriptsDict = GetScriptList(projectFilepath)
+
+    # Process each script
+    for guid, path in scriptsDict.items():
+        scriptXML = ET.SubElement(rootXML, "Script", guid = str(guid), path = path)
+        serializableMembers = GetSerializableMembers(path)
+        for memberLine in serializableMembers:
+            # Member should be in the form: {TYPE} {NAME} = {DEFAULTVALUE}
+            tokens = [x for x in memberLine.split(' ') if x is not '']
+            paramType = tokens[0]
+            paramName = tokens[1]
+            paramXML = ET.SubElement(scriptXML, "Param", type = paramType, name = paramName)
+            if (len(tokens) >= 4 and tokens[2] == "="):
+                paramDefaultValue = tokens[3]
+                paramXML.text = paramDefaultValue
+
+    # Write XML to file
+    tree = ET.ElementTree(rootXML)
+    tree.write("Schema.xml")
+
+
+BuildSchemas("Katamari.xml")
