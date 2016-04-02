c8fd513ffe932bc84c6148c7966ccc0246679093
Gwynneth Leece <gleece@gmail.com>
2016-04-02 14:46:17 -0700

    More serialization WIP

--- Engine/Include/Serialization/HierarchicalSerializer.h
+++ Engine/Include/Serialization/HierarchicalSerializer.h
@@ -1,34 +1,36 @@
 #pragma once
 
+#include <stack>
 #include <string>
 #include "tinyxml2.h"
 
+#include "Math/Algebra.h"
+#include "Rendering/Color.h"
+
+using std::stack;
 using std::string;
 using namespace tinyxml2;
 
 class HierarchicalSerializer
 {
 public:
+    void    PushScope(string name);
+    void    PopScope();
 
-    class Element
-    {
-    public:
-        void SetAttribute(string name, int value);
-        void SetAttribute(string name, float value);
-        void SetAttribute(string name, bool value);
-        void SetAttribute(string name, string value);
+    template<typename T>
+    void    SetAttribute(string name, T value);
+    void    SetAttributeVector3(Vector3 value);
+    void    SetAttributeColorRGB(ColorRGB value);
 
-    private:
-        XMLElement* m_element;
-    };
+    template<typename T>
+    void    AddLeaf(string name, string valueName, T value);
+    void    AddLeafVector3(string name, Vector3 value);
+    void    AddLeafColorRGB(string name, ColorRGB value);
 
-    HierarchicalSerializer();
-
-    Element*    NewElement(string name);
-
-    void        Save(string filepath);
-    void        Load(string filepath);
+    void    Save(string filepath);
+    void    Load(string filepath);
 
 private:
-    XMLDocument     m_document;
+    XMLDocument         m_document;
+    stack<XMLElement*>  m_elementStack;
 };
--- Engine/Src/Serialization/HierarchicalSerializer.cpp
+++ Engine/Src/Serialization/HierarchicalSerializer.cpp
@@ -1,5 +1,79 @@
 #include "Serialization/HierarchicalSerializer.h"
 
+void HierarchicalSerializer::PushScope(string name)
+{
+    XMLElement* newElement = m_document.NewElement(name.c_str());
+    XMLElement* parentElement = m_elementStack.top();
+    if (parentElement != NULL)
+    {
+        parentElement->InsertEndChild(newElement);
+    }
+    else
+    {
+        m_document.InsertEndChild(newElement);
+    }
+    m_elementStack.push(newElement);
+}
+
+void HierarchicalSerializer::PopScope()
+{
+    m_elementStack.pop();
+}
+
+template<typename T>
+void HierarchicalSerializer::SetAttribute(string name, T value)
+{
+    XMLElement* element = m_elementStack.top();
+    if (element != NULL)
+    {
+        element->SetAttribute(name.c_str(), value);
+    }
+}
+
+void HierarchicalSerializer::SetAttributeVector3(Vector3 value)
+{
+    XMLElement* element = m_elementStack.top();
+    if (element != NULL)
+    {
+        element->SetAttribute("x", value.x());
+        element->SetAttribute("y", value.y());
+        element->SetAttribute("z", value.z());
+    }
+}
+
+void HierarchicalSerializer::SetAttributeColorRGB(ColorRGB value)
+{
+    XMLElement* element = m_elementStack.top();
+    if (element != NULL)
+    {
+        element->SetAttribute("r", value.r);
+        element->SetAttribute("g", value.g);
+        element->SetAttribute("b", value.b);
+    }
+}
+
+template<typename T>
+void HierarchicalSerializer::AddLeaf(string name, string valueName, T value)
+{
+    PushScope(name);
+    SetAttribute(valueName, value);
+    PopScope();
+}
+
+void HierarchicalSerializer::AddLeafVector3(string name, Vector3 value)
+{
+    PushScope(name);
+    SetAttributeVector3(value);
+    PopScope();
+}
+
+void HierarchicalSerializer::AddLeafColorRGB(string name, ColorRGB value)
+{
+    PushScope(name);
+    SetAttributeColorRGB(value);
+    PopScope();
+}
+
 void HierarchicalSerializer::Save(string filepath)
 {
     m_document.SaveFile(filepath.c_str());
@@ -7,5 +81,5 @@ void HierarchicalSerializer::Save(string filepath)
 
 void HierarchicalSerializer::Load(string filepath)
 {
-
+    // TODO implement me
 }
