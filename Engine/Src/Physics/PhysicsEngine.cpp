44ea367d2dcaff5a741c2bb3a13af406c4c67e67
Gwynneth Leece <gleece@gmail.com>
2016-12-30 12:54:40 -0800

    Physics: generate rigid body contacts from collision contacts, where applicable

--- Engine/Src/Physics/PhysicsEngine.cpp
+++ Engine/Src/Physics/PhysicsEngine.cpp
@@ -1,6 +1,11 @@
-#include "Physics/CollisionEngine.h"
 #include "Physics/PhysicsEngine.h"
+
+#include "GameObjectBase.h"
+#include "Physics/Collider.h"
+#include "Physics/CollisionDetection.h"
+#include "Physics/CollisionEngine.h"
 #include "Physics/RigidBody.h"
+#include "Physics/RigidBodyContact.h"
 
 #include <algorithm>
 
@@ -53,7 +58,27 @@ void PhysicsEngine::ResolveCollisions(float deltaTime)
             break;
 
         CollisionContact collisionContact = collisionData->Contacts[i];
-        // TODO finish implementation
+        GameObjectBase* objectA = collisionContact.ColliderA->GetGameObject();
+        GameObjectBase* objectB = collisionContact.ColliderB->GetGameObject();
+        RigidBody* rigidBodyA = objectA->GetRigidBody();
+        RigidBody* rigidBodyB = objectB->GetRigidBody();
+
+        if (rigidBodyA == NULL && rigidBodyB == NULL)
+        {
+            // No rigid body in this collision, so do nothing
+            continue;
+        }
+
+        // Create a rigid body contact, since there is at least one rigid body involved
+        // One of the bodies might be NULL, but RigidBodyContact will handle this case
+        m_rigiBodyContacts[contactCount].Body[0] = rigidBodyA;
+        m_rigiBodyContacts[contactCount].Body[1] = rigidBodyB;
+        m_rigiBodyContacts[contactCount].ContactPoint = collisionContact.ContactPoint;
+        m_rigiBodyContacts[contactCount].ContactNormal = collisionContact.ContactNormal;
+        m_rigiBodyContacts[contactCount].Penetration = collisionContact.Penetration;
+        m_rigiBodyContacts[contactCount].Friction = 0.9f;       // TODO custom friction values
+        m_rigiBodyContacts[contactCount].Restitution = 0.1f;    // TODO custom restitution values
+        contactCount++;
     }
 
     // Resolve the collisions involving rigid bodies
